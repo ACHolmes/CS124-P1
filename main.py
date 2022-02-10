bb470672ef0a6787020ff42f5d800c23cdc8b6e2
DetectiveSh4rk <31936882+DetectiveSh4rk@users.noreply.github.com>
2022-02-10 14:38:00 -0500

    basic classes

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import numpy as np
 import matplotlib as plt
 
+class Edge:
+    def __init__(self, weight, node1, node2):
+        self.weight = weight
+        self.n1 = node1
+        self.n2 = node2
+
+class Vertex: 
+    def __init__(self, dim, pos = 0):
+        self.pos = pos if pos else dim * [0]
+        self.pos = pos
