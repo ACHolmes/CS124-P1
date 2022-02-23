1be0426fc65e71da31a2472a9725456e79c7806c
Andrew Holmes <acholmes001@gmail.com>
2022-02-23 15:33:30 -0500

    Added .exe files

---
+++ test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "mtwister.h"
+#include "mtwister.c"
+
+int main() {
+  MTRand r = seedRand(1337);
+  int i;
+  for(i=0; i<1000; i++) {
+    printf("%f\n", genRand(&r));
+  }
+  return 0;
+}
