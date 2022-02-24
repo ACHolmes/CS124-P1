d4928effb8293a74e8a7911ef574336a65fce563
Andrew Holmes <acholmes001@gmail.com>
2022-02-23 20:32:10 -0500

    timing function

--- randmst.c
+++ randmst.c
@@ -1,9 +1,12 @@
 #include "prim.h"
+#include "time.h"
 
 //#define nArray = [128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144]
 // Capped at 512, 1024 doesn't work
 int main(int argc, char* argv[])
 {
+    double time_spent = 0.0;
+    clock_t begin = clock();
     int nArray[] = {128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144};
     // sets random seed for rand functionality
     srand( time(NULL) );
@@ -46,5 +49,9 @@ int main(int argc, char* argv[])
     }
     average = average / numtrials;
     printf("Average: %f (%i, dim: %i, trials: %i)", average, numpoints, dimension, numtrials);
+    clock_t end = clock();
+    time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+ 
+    printf("The elapsed time is %f seconds", time_spent);
     return 0;
 } 
